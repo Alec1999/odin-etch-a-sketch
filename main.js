111a54649358750930aa6837b0ab230103127ffb
Alec1999 <a.ours1999@gmail.com>
2025-08-15 13:40:40 -0700

    Add functionality for any size grid to be added.
    Clean up sytax that is not being used.

--- main.js
+++ main.js
@@ -2,14 +2,11 @@ function createGridSquare() {
     const gridContainer = document.getElementById("grid-container");
     const newSquare = document.createElement("div");
     newSquare.classList.add("grid-square");
+    newSquare.style.height = ((800 / 16) + "px");
+    newSquare.style.width = ((800 / 16) + "px");
     gridContainer.appendChild(newSquare);
 }
 
-// for (i = 0; i < 16; i++) { // Should be number of columns
-for (j = 0; j < 1; j++) { // should be number of rows
+for (i = 0; i < (16 * 16); i++) { 
     createGridSquare();
 }
-//}
-    
-
- 
