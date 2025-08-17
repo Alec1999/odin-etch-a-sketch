e8df11ac3a29c240429eb31c7ed271444c78dea2
Alec1999 <a.ours1999@gmail.com>
2025-08-17 12:01:56 -0700

    Add ability for user to enter desired grid size.

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 const gridContainer = document.getElementById("grid-container");
+const newGridBtn = document.getElementById("buttons");
+const totalGridPx = 800;
+let currentGridSize = 16;
 
 function createGridSquare() {
     const newSquare = document.createElement("div");
     newSquare.classList.add("grid-square");
-    newSquare.style.height = ((800 / 16) + "px");
-    newSquare.style.width = ((800 / 16) + "px");
+    newSquare.style.height = ((totalGridPx / currentGridSize) + "px");
+    newSquare.style.width = ((totalGridPx / currentGridSize) + "px");
     gridContainer.appendChild(newSquare);
 }
 
-for (i = 0; i < (16 * 16); i++) { 
-    createGridSquare();
+function createNewGrid() {
+    // Create new grid size
 }
+
+function decideGridSize (currentGridSize) {
+    for (i = 0; i < (currentGridSize * currentGridSize); i++) { 
+        createGridSquare();
+    }
+}
+
 
 const exsistingSquare = document.getElementsByClassName("grid-square");
 for (const square of exsistingSquare) {
@@ -18,3 +28,9 @@ for (const square of exsistingSquare) {
         square.style.backgroundColor = "black";
     });
 };
+
+newGridBtn.addEventListener("click", (e) => {
+    currentGridSize = prompt("Enter grid size");
+});
+
+decideGridSize(currentGridSize);
