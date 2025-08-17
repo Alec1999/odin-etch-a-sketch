cbbd820260fc267ba8bb7ef2648e7ca1e2ddbacd
Alec1999 <a.ours1999@gmail.com>
2025-08-16 22:17:35 -0700

    Add color change when mousing over each square.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+const gridContainer = document.getElementById("grid-container");
+
 function createGridSquare() {
-    const gridContainer = document.getElementById("grid-container");
     const newSquare = document.createElement("div");
     newSquare.classList.add("grid-square");
     newSquare.style.height = ((800 / 16) + "px");
@@ -10,3 +11,10 @@ function createGridSquare() {
 for (i = 0; i < (16 * 16); i++) { 
     createGridSquare();
 }
+
+const exsistingSquare = document.getElementsByClassName("grid-square");
+for (const square of exsistingSquare) {
+    square.addEventListener("mouseenter", (e) => {
+        square.style.backgroundColor="black";
+    });
+};
